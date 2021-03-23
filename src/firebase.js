01c25d6f2314252f143b65ee118b7d7e8e872b3a
Nikki <nikki.satopdx@gmail.com>
2021-03-22 20:01:50 -0700

    finished header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCSQ9zDrtL9yAju4fPwYnzJJch5h9SdZ08',
+  authDomain: 'tinder-clone-7c3e2.firebaseapp.com',
+  projectId: 'tinder-clone-7c3e2',
+  storageBucket: 'tinder-clone-7c3e2.appspot.com',
+  messagingSenderId: '550874617375',
+  appId: '1:550874617375:web:42920e478b970a0fba672a',
+};
